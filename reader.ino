7165c90222232fcfe6913512946e1a0e0eebc390
Neil Ramsbottom <nramsbottom@hotmail.com>
2020-10-25 02:15:15 +0200

    Added Arduino sketch for reader.

---
+++ reader.ino
@@ -0,0 +1,40 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+MFRC522 mfrc522(10, 9); // MFRC522 mfrc522(SS_PIN, RST_PIN)
+
+void setup() {
+
+  Serial.begin(9600);
+  SPI.begin();      // Init SPI bus
+  mfrc522.PCD_Init();   // Init MFRC522
+
+}
+
+void loop() {
+
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+
+  //Reading from the card
+  String tag = "";
+  for (byte i = 0; i < mfrc522.uid.size; i++)
+  {
+    tag.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? "0" : ""));
+    tag.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  tag.toUpperCase();
+
+  Serial.print("CARD");
+  Serial.print("|");
+  Serial.print(tag);
+  Serial.print("\r\n");
+  delay(2000);
+}
